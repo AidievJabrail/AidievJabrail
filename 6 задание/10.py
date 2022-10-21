c56737b06ae57ce8e412a5671ce4367873f021d8
AidievJabrail <95432902+AidievJabrail@users.noreply.github.com>
2022-10-21 18:52:30 +0300

    1

---
+++ 6 задание/10.py
@@ -0,0 +1,16 @@
+from turtle import *
+screensize(10000, 10000)
+tracer(0)
+k = 50
+for i in range(7):
+    goto(xcor() + 6 * k, ycor() - 9 * k)
+    goto(xcor() - 6 * k, ycor() + 2 * k)
+    goto(xcor() + 12 * k, ycor() + 3 * k)
+pu()
+for x in range(-30, 30):
+    for y in range(-30, 30):
+        goto(x * k, y * k)
+        dot(4)
+update()
+done()
+print(7 * 10)
