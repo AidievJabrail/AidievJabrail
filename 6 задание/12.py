64edec050786e87c4ad983d3a0a05863abcd3d99
AidievJabrail <95432902+AidievJabrail@users.noreply.github.com>
2022-10-22 11:10:22 +0300

    1

--- 6 задание/12.py
+++ 6 задание/12.py
@@ -1,15 +1,17 @@
 from turtle import *
 screensize(10000, 10000)
 tracer(0)
-k = 50
-for i in range(10):
-    goto(xcor() - 6 * k, ycor() + 9 * k)
-    goto(xcor() + 6 * k, ycor() - 2 * k)
-    goto(xcor() - 3 * k, ycor() - 6 * k)
+k = 10
+left(90)
+fd (200 * k)
+for i in range(4 ):
+    rt(90)
+    fd(100 * k)
 pu()
-for x in range(-40, 30):
-    for y in range(-30, 30):
+for x in range(60):
+    for y in range(201):
         goto(x * k, y * k)
-        dot(4)
+        dot(3)
 update()
 done()
+print(99 * 99 + 100 * 5)
