dd684446e8116ceb2e60fce5e434ca5560857626
AidievJabrail <95432902+AidievJabrail@users.noreply.github.com>
2023-03-09 11:31:21 +0300

    1

--- 23 Задание/115.py
+++ 23 Задание/115.py
@@ -8,5 +8,15 @@ for i in range(2, 112):
     if i % 3 == 0:
         mn = min(a[i // 3], mn)
     a[i] += mn + 1
-print(a[111])
+mn = a[111]
+def f(a, b, c):
+    if a > b or c > 6:
+        return 0
+    if a == b and  c == 6:
+        return 1
+    return f(a + 1, b, c + 1) + f(a + 5, b, c + 1) + f(a * 3, b, c + 1)
+print(f(1, 111, 0))
+
+
+
 
